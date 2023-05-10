a1f491f412e41e5568903b496dbd9c435310be65
faridagharib1234 <58792738+faridagharib1234@users.noreply.github.com>
2023-05-11 02:23:08 +0300

    Add files via upload

--- CodeWithGUI.py
+++ CodeWithGUI.py
@@ -1,84 +1,87 @@
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import messagebox, simpledialog
 
 
 def input_box(prompt):
-    """
-    Displays a simple dialog box for user input and returns the input as a string.
-    """
     root = tk.Tk()
     root.withdraw()
     user_input = simpledialog.askstring("Input", prompt)
     return user_input
 
 
-def is_safe(available, allocated, max_resources, process_id, request):
-    """
-    Determines whether allocating the requested resources to the specified process would result in a safe state.
-    """
-    # Calculate the need matrix
-    need = [list(map(lambda x, y: x - y, max_resources[i], allocated[i])) for i in range(len(max_resources))]
-
-    # Check if the request is valid
-    for j in range(len(available)):
-        if request[j] > need[process_id][j]:
-            print("Error: The requested resources exceed the maximum need.")
-            return False
-        if request[j] > available[j]:
-            print("Error: The requested resources are not available.")
-            return False
-
-    # Try to allocate the resources and update the available, allocated, and need matrices
-    temp_available = available.copy()
-    temp_allocated = [row[:] for row in allocated]
-    temp_need = [row[:] for row in need]
-    for j in range(len(available)):
-        temp_available[j] -= request[j]
-        temp_allocated[process_id][j] += request[j]
-        temp_need[process_id][j] -= request[j]
-
-    # Initialize the finish matrix to false for all processes
-    finish = [False] * len(max_resources)
-
-    # Try to find a safe sequence using a modified version of the Banker's algorithm
-    work = temp_available.copy()
-    while True:
-        found = False
-        for i in range(len(max_resources)):
-            if not finish[i] and all(temp_need[i][j] <= work[j] for j in range(len(available))):
-                finish[i] = True
-                work = [work[j] + temp_allocated[i][j] for j in range(len(available))]
-                found = True
-                break
-        if not found:
-            break
-
-    # If a safe sequence was found, the request is safe; otherwise, it's unsafe
-    if all(finish):
-        print("The request is safe.")
-        return True
-    else:
-        print("The request is unsafe.")
-        return False
+def display_message(message):
+    messagebox.showinfo("Message", message)
 
 
-# Get input from the user
-try:
+def run_banker_algorithm():
     num_processes = int(input_box("Enter the number of processes: "))
     num_resources = int(input_box("Enter the number of resources: "))
-    available = [int(x) for x in input_box("Enter total resources: ").split()]
-    allocated = []
-    max_resources = []
+
+    # Get available resources
+    available_str = input_box("Enter the number of available resources (separated by commas): ")
+    available = list(map(int, available_str.split(",")))
+
+    # Get maximum demand of each process
+    max_demand = []
     for i in range(num_processes):
-        allocated.append([int(x) for x in input_box(f"Enter current allocation for process {i}: ").split()])
-        max_resources.append([int(x) for x in input_box(f"Enter maximum need for process {i}: ").split()])
-    process_id = int(input_box(f"Enter process id (0-{num_processes - 1}): "))
-    request = [int(x) for x in input_box("Enter requested resources: ").split()]
+        max_demand_str = input_box(f"Enter the maximum resource demand of process {i} (separated by commas): ")
+        max_demand.append(list(map(int, max_demand_str.split(","))))
 
-    # Check if the request is safe or unsafe
-    is_safe(available, allocated, max_resources, process_id, request)
+    # Get resources allocated to each process
+    allocation = []
+    for i in range(num_processes):
+        allocation_str = input_box(f"Enter the resources allocated to process {i} (separated by commas): ")
+        allocation.append(list(map(int, allocation_str.split(","))))
 
-except ValueError:
-    print("Error: Invalid input.")
-except IndexError:
-    print("Error: Process ID is out of range.")
+    # Calculate the need of each process
+    need = []
+    for i in range(num_processes):
+        need.append([max_demand[i][j] - allocation[i][j] for j in range(num_resources)])
+
+    # Initialize the finish array to false for each process
+    finish = [False] * num_processes
+
+    # Initialize the work and finish arrays to the current available resources
+    work = available.copy()
+
+    # Initialize a list to hold the safe sequence, if one exists
+    safe_sequence = []
+
+    # Run the algorithm until either a safe sequence is found or it is determined that no safe sequence exists
+    while True:
+        # Find an i such that both:
+        # - The ith process is not finished (finish[i] == False)
+        # - The ith process has a need that is less than or equal to the work for all resources (need[i][j] <= work[j])
+        i = None
+        for p in range(num_processes):
+            if not finish[p] and all(need[p][j] <= work[j] for j in range(num_resources)):
+                i = p
+                break
+
+        # If no such i exists, break out of the loop and check if all processes are finished
+        if i is None:
+            if all(finish):
+                display_message(f"Safe sequence: {safe_sequence}")
+                break
+            else:
+                display_message("No safe sequence exists.")
+                break
+
+        # If an i was found, add it to the safe sequence and update the work array
+        safe_sequence.append(i)
+        for j in range(num_resources):
+            work[j] += allocation[i][j]
+        finish[i] = True
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Banker's Algorithm")
+
+    title_label = tk.Label(root, text="Banker's Algorithm", font=("Arial Bold", 16))
+    title_label.pack(pady=10)
+
+    run_button = tk.Button(root, text="Run", command=run_banker_algorithm)
+    run_button.pack(pady=10)
+
+    root.mainloop()
